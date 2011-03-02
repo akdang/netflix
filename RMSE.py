f9ffc4e5786f4e69338ca04f38dc21fa11f62346
akdang <akdang@gmail.com>
2011-03-02 15:13:14 -0600

    modified RMSE for lists

---
+++ RMSE.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# -------
+# RMSE.py
+# -------
+
+#print "RMSE.py"
+
+def rmse (a, p) :
+    assert type(a) == list
+    assert type(p) == list
+    assert len(a) == len(p)
+    i = 0
+    s = len(a)
+    w = 0
+    while i != s :
+        v = float(a[i]) - float(p[i])
+        w += (v ** 2)
+        i += 1
+    assert type(w) is float
+    assert 0 <= w <= (16 * s)
+    m = (w / s)
+    assert type(m) is float
+    assert 0 <= m <= 16
+    r = (m ** .5)
+    assert type(r) is float
+    assert 0 <= r <= 4
+    return r
+
+#assert str(rmse((3, 3, 3), (3, 3, 3))) == "0.0"
+#assert str(rmse((1, 1, 1, 1), (5, 5, 5, 5))) == "4.0"
+#assert str(rmse((5, 3, 2, 4, 5), (2, 4, 3, 1, 2))) == "2.40831891576"
+
+#print "Done."
