a1076bffa8759c2cace1f85f58d7a0f91c073ad8
akdang <akdang@gmail.com>
2011-03-04 22:55:48 -0600

    new RMSE with map

--- RMSE.py
+++ RMSE.py
@@ -25,3 +25,26 @@ def rmse (a, p) :
     assert type(r) is float
     assert 0 <= r <= 4
     return r
+    
+def square_of_difference(x, y) :
+  rating_dict = {'1' : 1, '2' : 2, '3' : 3, '4' : 4, '5' : 5}
+  actual = rating_dict[x]
+  assert type(actual) is int
+  pred = float(y)
+  sd = (actual - pred) ** 2
+  assert type(sd) is float
+  return sd
+  
+def mean(a) :
+  assert type(a) is list
+  m = sum(a) / len(a)
+  assert 0 <= m <= 16
+  return m
+    
+def rmse2(a,p) :
+  assert type(a) is list
+  assert type(p) is list
+  assert len(a) == len(p) 
+  r = mean(map(square_of_difference, a, p)) ** .5
+  assert 0 <= r <= 4
+  return r
